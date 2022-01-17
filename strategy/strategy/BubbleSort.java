82b594930891677e09b1b9696d337c6323fe12dc
Abhinav110102 <abhinavmyadala@gmail.com>
2022-01-16 22:26:17 -0500

    Commit2

--- strategy/strategy/BubbleSort.java
+++ strategy/strategy/BubbleSort.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
  * BubbleSort implements SortBehavior class and we also create a algorithm that sorts the items in the list through BubbleSort 
  * Sources I used for this algorithm are : https://www.youtube.com/watch?v=uJLwnsLn0_Q a video about BubbleSort,
  * and https://www.geeksforgeeks.org/sorting-strings-using-bubble-sort-2/ this website help me understand the simple bubbleSort and remeber what we learned in CSCE 146, 
- * and https://javarevisited.blogspot.com/2014/08/bubble-sort-algorithm-in-java-with.html#axzz7IBhOKsRw This blog had multiple example in which bubble sort was used 
+ * and https://javarevisited.blogspot.com/2014/08/bubble-sort-algorithm-in-java-with.html#axzz7IBhOKsRw This blog had multiple examples in which bubble sort was used.
  */
 
 public class BubbleSort implements SortBehavior {
--- strategy/strategy/InsertionSort.java
+++ strategy/strategy/InsertionSort.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
  * @author Abhinav
  * InsertionSort implements SortBehavior class and we also create a algorithm that sorts the items in the list through InsertionSort 
  * Sources I used for this for this algorithm: https://www.youtube.com/watch?v=4P95xpLjbQ4&t=306s This video talks about insertionSort with String ArrayList,
- * https://www.geeksforgeeks.org/insertion-sort/ and the video on the page , and https://www.java67.com/2014/09/insertion-sort-in-java-with-example.html this website explain and give multiple example of how to do insertion sort in multiple cases. 
+ * https://www.geeksforgeeks.org/insertion-sort/ and the video on the page , and https://www.java67.com/2014/09/insertion-sort-in-java-with-example.html this website explain and give multiple examples of how to do insertion sort in multiple cases. 
  */
 public class InsertionSort implements SortBehavior {
     public ArrayList<String> sort(ArrayList<String> data){
